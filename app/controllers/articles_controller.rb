17888728e4272d5cb8c7b8e9ddd33b59511f3e92
Yash Gupta <yash.gupta091510bi@gmail.com>
2016-05-27 01:54:19 +0530

    user article association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ end
 
 def create
 @article= Article.new(article_params)
+@article.user=User.first
 if @article.save
   flash[:success]="Article is successfully created"
 redirect_to article_path(@article)
